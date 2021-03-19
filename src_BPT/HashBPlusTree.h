145d932592bdf3806799f63b08c3b66aadc70dd1
Rainy-Memory- <2630737606@qq.com>
2021-03-19 12:43:11 +0800

    test best size

--- src_BPT/HashBPlusTree.h
+++ src_BPT/HashBPlusTree.h
@@ -83,7 +83,7 @@ public:
     };
 
 private:
-    RainyMemory::BPlusTree<hashType, int, 1000, 1000> bpt;
+    RainyMemory::BPlusTree<hashType, int, 500, 500> bpt;
 
 public:
     explicit BPlusTree(const string &_filename) : bpt(_filename) {}
